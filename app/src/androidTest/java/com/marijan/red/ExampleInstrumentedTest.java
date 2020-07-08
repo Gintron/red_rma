1bb025d6b6195594ae5e63193d0a22a5bd6d6035
gintron <marijan10vk@gmail.com>
2020-07-08 23:34:15 +0200

    red

--- app/src/androidTest/java/com/marijan/red/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/marijan/red/ExampleInstrumentedTest.java
@@ -1,7 +1,7 @@
 package com.marijan.red;
 
 import android.content.Context;
-import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.platform.app.InstrumentationRegistry ;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
@@ -19,8 +19,8 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.koddev.instagramtest", appContext.getPackageName());
+        assertEquals("com.marijan.red", appContext.getPackageName());
     }
 }
